16deb44932fa5f707598bf328188bc0b0558b0bc
Jukka-Pekka Virtanen <virtanen@jukk.is>
2024-12-15 12:57:04 +0200

    Add tests

--- tests/lib/test_matrix.py
+++ tests/lib/test_matrix.py
@@ -2,14 +2,20 @@ from aoc.lib.matrix import Matrix, Point
 
 def test_matrix():
     t = """\
-1234
-5678"""
+12
+34"""
 
     m = Matrix.parse_matrix(t, conversion=int)
     print(str(m))
 
     assert m.get((0, 0)) == 1
+    assert m.get((1, 0)) == 2
+    assert m.get((0, 1)) == 3
+    assert m.get((1, 1)) == 4
     assert m.contains_point((0, 0))
+
+    for a, b in zip(m, [[1, 2], [3, 4]]):
+        assert a == b
 
     m = Matrix.empty_matrix(10, 10, 0)
     m.get((0, 0))
